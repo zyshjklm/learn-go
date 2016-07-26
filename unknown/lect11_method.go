5af2f429d2ac19bbb784bf09a1b8d87ebb532af9
jungle <jungle85gopy@gmail.com>
2016-07-26 22:18:50 +0800

    update lect11_method.go for unknown/

--- unknown/lect11_method.go
+++ unknown/lect11_method.go
@@ -23,11 +23,35 @@ func (b B) Print() {
 func (a *A) PrintName() {
 	a.Name = "AAA"
 	fmt.Println("A")
+	fmt.Println(a.Name)
 }
 
 func (b B) PrintName() {
 	b.Name = "BBB"
 	fmt.Println("B")
+	fmt.Println(b.Name)
+}
+
+type TINT int 
+
+func (tint *TINT) Print() {
+	fmt.Println("TINT...")
+}
+// 通过type来定义自己的Int, 并实现相应的method。
+
+func (tint *TINT) Increase() {
+	*tint += (TINT)(100)
+	fmt.Println(*tint)
+}
+
+type Mod struct {
+	Name string
+}
+
+// method access type field
+func (m *Mod) ModifyField() {
+	m.Name = "modify-by-method"
+	fmt.Println(m.Name)
 }
 
 func main() {
@@ -44,4 +68,22 @@ func main() {
 	b1 := B{}
 	b1.PrintName()
 	fmt.Println("b1.Name:", b1.Name)
+	// PrintName not modify b1.Name
+
+	var t TINT
+	t.Print()
+	(*TINT).Print(&t)
+
+	// modify field
+	m := Mod{}
+	m.ModifyField()
+	m.Name = "modify-by-main"
+	fmt.Println(m.Name)
+
+	var t1 TINT
+	t1 = 3
+	fmt.Println(t1)
+	t1.Increase()
+	fmt.Println(t1)
 }
+
