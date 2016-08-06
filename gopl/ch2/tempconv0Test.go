df3b457accd570c615cc3edad0d7c619baec23dd
jungle <jungle85gopy@gmail.com>
2016-08-06 17:12:31 +0800

    add gopl/ch2/temconv*

---
+++ gopl/ch2/tempconv0Test.go
@@ -0,0 +1,17 @@
+// test tempconv
+package main
+
+import (
+    "github.com/learn-go/gopl/ch2/tempconv0"
+    "fmt"
+)
+
+func main() {
+    fmt.Printf("%g\n", tempconv.BoilingC - tempconv.FreezingC)
+    boilingF := tempconv.CToF(tempconv.BoilingC)
+    fmt.Println(tempconv.BoilingC.String())
+    fmt.Printf("%g\n", boilingF - tempconv.CToF(tempconv.FreezingC))
+
+
+}
+
