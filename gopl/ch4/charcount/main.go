3230698667807bd59f1ecd9945b4925c7046023b
jungle <jungle85gopy@gmail.com>
2016-09-04 23:42:12 +0800

    add gopl/ch4/charcount/main.go

---
+++ gopl/ch4/charcount/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"io"
+	"os"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	counts := make(map[rune]int)	// counts of Unicode char
+	var utflen [utf8.UTFMax + 1]int	// count of len of UTF-8 encodings
+	invalid := 0		// count of invalid UTF-8 char
+
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, n, err := in.ReadRune()	// return rune, nbytes, err
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+
+	fmt.Printf("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+}
+
+
+
