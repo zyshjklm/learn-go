d0b071cd63220aa975c101d93dd91dd22f348c32
jungle <jungle85gopy@gmail.com>
2016-09-04 10:43:28 +0800

    modify gopl/ch4/in-place/main.go for exer 4.5

--- gopl/ch4/in-place/main.go
+++ gopl/ch4/in-place/main.go
@@ -20,9 +20,27 @@ func dupAdjacent(str []string) []string {
 	return result
 }
 
+func dupAdjacent2(str []string) []string {
+	result := make([]string, len(str), len(str))
+	result[0] = str[0]
+	i := 0
+
+	for _, s := range str {
+		if result[i] != s {
+			i++
+			result[i] = s
+		}
+	}
+	return result[:i+1]
+}
+
 func main() {
 	testStr := [...]string{"1", "1", "1", "2", "2", "3"}
 	fmt.Println(testStr)
 	newStr := dupAdjacent(testStr[:])
 	fmt.Println(newStr)
+
+	testStr2 := [...]string{"1", "1", "1", "2", "2", "3"}
+	newStr2 := dupAdjacent2(testStr2[:])
+	fmt.Println(newStr2)
 }
