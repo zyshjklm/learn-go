bfeab682aee61566bc972649393a8c0049b71189
jungle <jungle85gopy@gmail.com>
2016-09-07 23:24:31 +0800

    add gopl/ch4/issues/main.go

---
+++ gopl/ch4/issues/main.go
@@ -0,0 +1,44 @@
+// Issues prints a tble of GitHub issues matching the search terms
+package main 
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/learn-go/gopl/ch4/github"
+
+)
+
+/*
+refer:
+	https://developer.github.com/v3/search/#search-issues
+os.Args like:
+	windows label:bug language:python state:open
+
+example:
+	go run issues/main.go linux language:go
+
+*/
+
+func main() {
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%d issues:\n", result.TotalCount)
+
+	for _, item := range result.Items {	
+		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+	}
+}
+
+// output:
+/*
+q:  linux+language%3Ago+state%3Aopen
+7258 issues:
+#10      ianomad Windows/Linux build
+#117     mskogly Linux 32
+#2        jbenet Linux?
+#128   mveytsman Defaults for Amazon Linux
+*/
