89002cf3f4d1552f222e4c5b0333eb30589f0f6b
jungle <jungle85gopy@gmail.com>
2016-09-10 11:22:02 +0800

    add gopl/ch4/issuesreport/main.go

---
+++ gopl/ch4/issuesreport/main.go
@@ -0,0 +1,56 @@
+package main 
+
+import (
+	
+	"log"
+	"os"
+	"time"
+	"text/template"
+	"github.com/learn-go/gopl/ch4/github"
+
+)
+
+const templ = `{{.TotalCount}} issues:
+{{range .Items}}--------------------------------
+Number: {{.Number}}
+User: {{.User.Login}}
+Title: {{.Title | printf "%.64s"}}
+Age: {{.CreatedAt | daysAgo}} days
+{{end}}`
+
+func daysAgo(t time.Time) int {
+	return int(time.Since(t).Hours() / 24)
+}
+
+func main() {
+	var report = template.Must(
+		template.New("issuelist").
+		Funcs(template.FuncMap{"daysAgo": daysAgo}).
+		Parse(templ))
+
+	result, err := github.SearchIssues(os.Args[1:])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := report.Execute(os.Stdout, result); err != nil {
+		log.Fatal(err)
+	}
+}
+
+/*
+// output:
+go run issuesreport/main.go repo:golang/go is:open json decoder
+
+16 issues:
+--------------------------------
+Number: 11046
+User: kurin
+Title: encoding/json: Decoder internally buffers full input
+Age: 464 days
+--------------------------------
+Number: 15314
+User: okdave
+Title: proposal: some way to reject unknown fields in encoding/json.Dec
+Age: 147 days
+*/
