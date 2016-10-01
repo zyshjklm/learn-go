37d133f57b1942144d4923c7deef6c764484eb79
jungle <jungle85gopy@gmail.com>
2016-10-01 14:51:53 +0800

    add tour/helloCode/stringutil/reverse.go for hello/

--- tour/helloCode/hello/hello.go
+++ tour/helloCode/hello/hello.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "fmt"
+    "github.com/jungle85gopy/learn-go/tour/helloCode/stringutil"
 )
 
 func main() {
     fmt.Printf("Hello Code World!\n")
+    fmt.Printf(stringutil.Reverse("\n!dlrow olleH"))
 }
