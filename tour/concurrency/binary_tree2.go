2fcd983ec88b90a798ff5d884cb7cf14f322de74
jungle <jungle85gopy@gmail.com>
2016-10-05 12:37:33 +0800

    use goroutine to finish tour/concurrency/binary_tree2.go

--- tour/concurrency/binary_tree2.go
+++ tour/concurrency/binary_tree2.go
@@ -33,9 +33,49 @@ func Walk_test() {
 	fmt.Println("\n")	
 }
 
+// determines whether the trees 
+// t1 an t2 contains the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	var ch1, ch2 = make(chan int), make(chan int)
+
+	go func() {
+		Walk(t1, ch1)
+		close(ch1)
+	}()
+
+	go func() {
+		Walk(t2, ch2)
+		close(ch2)
+	}()
+
+	for {
+		c1, ok1 := <- ch1
+		c2, ok2 := <- ch2
+		if ok1 != true || ok2 != true {
+			break	// channel closed
+		}
+		fmt.Printf("  ch1: %d, ch2: %d\n", c1, c2)
+		if c1 != c2 { 
+			return false 
+		}
+	}
+	return true
+}
 
 
 func main() {
 	Walk_test()
 	
+	var t1, t2, t3 *tree.Tree
+	
+	t1 = tree.New(1)
+	t2 = tree.New(1)
+	t3 = tree.New(2)
+	fmt.Println("t1:", t1)
+	fmt.Println("t2:", t2)
+	fmt.Println("t3:", t3)
+
+	fmt.Printf("is t1 same to t2? %v\n", Same(t1, t2))	
+	fmt.Printf("is t1 same to t3? %v\n", Same(t1, t3))
+
 }
