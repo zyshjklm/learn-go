4b59ec84b0686b62da91203c95bf6ab5029514b4
jungle <jungle85gopy@gmail.com>
2017-07-17 23:03:11 +0800

    add 51reboot/day7/http/http-uri.go

---
+++ 51reboot/day7/http/http-uri.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+)
+
+func main() {
+	s := os.Args[1]
+	u, err := url.Parse(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("scheme:", u.Scheme)
+	fmt.Println("Host:", u.Host)
+	fmt.Println("Hostname:", u.Hostname)
+	fmt.Println("path:", u.Path)
+	fmt.Println("queryString:", u.RawQuery)
+	fmt.Println("user:", u.User)
+	fmt.Println("xx", u.Fragment)
+}
+
+/*
+go run http-url.go "http://xxx.com/path/x.jpg?abcd=2342"
+scheme: http
+Host: xxx.com
+path: /path/x.jpg
+queryString: abcd=2342
+user: <nil>
+xx
+*/
