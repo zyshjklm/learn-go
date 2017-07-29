9d4c3414ed5b75d4edf204225b5cbeebdfd53c94
jungle <jungle85gopy@gmail.com>
2017-07-29 11:35:53 +0800

    add 51reboot/day9/pool/;pool4.go -> pool_range.go

--- 51reboot/day9/pool/pool_range.go
+++ 51reboot/day9/pool/pool_range.go
@@ -35,12 +35,17 @@ func main() {
 		"http://qq.com"}
 	urlNum := len(urls)
 
-	for i := 0; i < urlNum; i++ {
+	// 协程数量比数据多。一个channel，可以被多个协程竞争抢到
+	// 就像一块骨头被几只狗抢。
+	// 调度器会尽量保证调度的公平性。调度的算法及其实现
+	// 这是生产-消费模式，也像用户通过请求到达网站入口，被调度到不同后端机器
+	for i := 0; i < urlNum+2; i++ {
 		go work(ch)
 	}
 	for i := 0; i < urlNum; i++ {
 		ch <- urls[i]
 	}
+	// 启动不要用Sleep来进行协程的同步。一定会出问题。前面只是演示
 	time.Sleep(2 * time.Second)
 	close(ch)
 }
