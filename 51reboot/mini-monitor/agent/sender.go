ee474c8ac014edfeebb773eb0fad3ae605351d59
jungle <jungle85gopy@gmail.com>
2017-08-20 16:00:53 +0800

    modify 51reboot/mini-monitor/agent/seder.go for retry

--- 51reboot/mini-monitor/agent/sender.go
+++ 51reboot/mini-monitor/agent/sender.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"github.com/jungle85gopy/learn-go/51reboot/mini-monitor/common"
 )
@@ -29,20 +31,57 @@ func (s *Sender) Channel() chan *common.Metric {
 	return s.ch
 }
 
+// connect retry connect to transfer.
+func (s *Sender) connect() net.Conn {
+	baseGap := 100 * time.Microsecond
+	for {
+		conn, err := net.Dial("tcp", s.addr)
+		if err != nil {
+			log.Print(err)
+			time.Sleep(baseGap)
+			baseGap *= 2
+			if baseGap > time.Second*30 {
+				baseGap = time.Second * 30
+			}
+			continue
+		}
+		log.Printf("local addr:%s\n", conn.LocalAddr())
+		return conn
+	}
+}
+
+// reConnect retry connect while write to conn err
+func (s *Sender) reConnect(conn net.Conn) *bufio.Writer {
+	conn.Close()
+	conn = s.connect()
+	w := bufio.NewWriter(conn)
+	return w
+}
+
 // Start 建立连接；
 // 循环从ch中读取metric，序列化metric，发送数据
 func (s *Sender) Start() {
 	var conn net.Conn
-	conn, err := net.Dial("tcp", s.addr)
-	if err != nil {
-		panic(err)
-	}
+	conn = s.connect()
+	w := bufio.NewWriter(conn)
+
+	ticker := time.NewTicker(time.Second * 5)
 	for {
-		metric := <-s.ch
-		buf, _ := json.Marshal(metric)
-		_, err := fmt.Fprintf(conn, "%s\n", buf)
-		if err != nil {
-			log.Printf("send metric err:%s", err.Error())
+		select {
+		case metric := <-s.ch:
+			buf, _ := json.Marshal(metric)
+			_, err := fmt.Fprintf(w, "%s\n", buf)
+			if err != nil {
+				log.Printf("Fprintf to remote err:%s", err.Error())
+				w = s.reConnect(conn)
+			}
+		case <-ticker.C:
+			// log.Print("-- Flush data to transfer from bufio of conn.")
+			err := w.Flush()
+			if err != nil {
+				log.Printf("Flush to remote err:%s", err.Error())
+				w = s.reConnect(conn)
+			}
 		}
 	}
 }
