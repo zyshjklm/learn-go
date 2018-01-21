f663a48ff8ca1b79a9abb71cb20129275634f5c5
jungle <jungle85gopy@gmail.com>
2018-01-21 23:34:59 +0800

    update day2/myEchoArgs2/main.go for flag.StringVar()

--- 51reboot/day2/myEchoArgs2/main.go
+++ 51reboot/day2/myEchoArgs2/main.go
@@ -8,6 +8,7 @@ import (
 
 var newline = flag.Bool("n", false, "end with \n\t for string")
 var sep = flag.String("s", " ", "separator")
+var myType string
 
 // sep 的类型是推导出来的，是字符串指针
 // -- help的结果：
@@ -16,6 +17,7 @@ var sep = flag.String("s", " ", "separator")
 //        separator (default " ")
 
 func main() {
+	flag.StringVar(&myType, "t", "test", "test, prod, preview")
 	flag.Parse()
 	//fmt.Print(strings.Join(flag.Args(), *sep))
 
