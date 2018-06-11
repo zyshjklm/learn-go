e2fc9d23c9f5a1588fe19ade8f1805a9107a4c40
jungle <jungle85gopy@gmail.com>
2018-06-11 22:28:42 +0800

    update restful/basic/basic_router1.go for router

--- restful/basic/basic_router1.go
+++ restful/basic/basic_router1.go
@@ -13,9 +13,33 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
 
+func todoIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Todo Index!")
+}
+
+func todoShow(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	todoID := vars["todoId"]
+	fmt.Fprintf(w, "Todo Show:%s", todoID)
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", handleRoot)
+	router.HandleFunc("/todos", todoIndex)
+	router.HandleFunc("/todos/{todoId}", todoShow)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
+
+/*
+curl localhost:8080/todos
+Todo Index!%
+
+curl localhost:8080/todos/23
+Todo Show:23%
+
+curl localhost:8080/todos/ab
+Todo Show:ab%
+
+*/
